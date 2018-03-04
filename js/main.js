3b5c567a7c1c0bcc88981e1abfe27270af206bd5
Your Name <you@example.com>
2018-03-04 17:42:16 +0600

    main

---
+++ js/main.js
@@ -0,0 +1,14 @@
+$.FullScreen({
+
+			move: '.wrapper',
+			row: '.row',
+			nav: '.nav',
+			time: '1s',
+			type: 'cubic-bezier(1,-0.12,0.44,0.99',
+
+			navClass: 'cursor',
+
+			minHeight:200
+
+
+		});
